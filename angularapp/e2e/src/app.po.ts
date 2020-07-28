4cf6190bc562f7b3468211b741beb7da973631ea
Alex <lehajusko@gmail.com>
2020-07-28 21:17:11 +0700

    Some changes for accounting project

--- angularapp/e2e/src/app.po.ts
+++ angularapp/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('wfm-root .content span')).getText() as Promise<string>;
   }
 }
--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wfm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularapp';
 }
