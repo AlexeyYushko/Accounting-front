1c1a85a2702f580b8d8a70714a2ecce706355b7c
Alex <lehajusko@gmail.com>
2020-07-29 21:57:12 +0700

    Adds login form validation

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +11,6 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AuthModule,
     AppRoutingModule
